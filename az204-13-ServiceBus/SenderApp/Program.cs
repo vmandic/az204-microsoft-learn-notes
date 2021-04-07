cc46133a43d2cfd89c410f03991dd72e25b6ec29
Vedran Mandic <mandic.vedran@gmail.com>
2021-04-07 22:05:47 +0200

    14 wrapped up, fun SB

---
+++ az204-13-ServiceBus/SenderApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace SenderApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting app...");
+
+            if (!System.IO.File.Exists("serviceBusConnectionString"))
+            {
+                throw new System.IO.FileNotFoundException("Could not find required file 'serviceBusConnectionString'");
+            }
+
+            var sbConnectionString = System.IO.File.ReadAllText("serviceBusConnectionString");
+            Console.WriteLine("Using service bus connection string: " + sbConnectionString);
+
+            // NOTE: send the message to SB "salesmessages" queue
+            var queueClient = new QueueClient(sbConnectionString, "salesmessages");
+
+            string message = "Sure would like a large pepperoni!";
+            var encodedMessage = new Message(System.Text.Encoding.UTF8.GetBytes(message));
+
+            await queueClient.SendAsync(encodedMessage);
+
+            // NOTE: login to azure portal and under Service Bus Queue explorer observe that the message is enqueued
+
+            Console.WriteLine("Message sent, check in Azure Portal, press any key to exit app...");
+            Console.ReadKey();
+        }
+    }
+}
