f246b486f2e84e31a51299203920cc5f5d118ba7
Vedran Mandic <mandic.vedran@gmail.com>
2021-04-09 22:13:42 +0200

    done

---
+++ az204-18/PhotoSharingApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+
+namespace PhotoSharingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            Console.WriteLine("Hello World!");
+
+            // Get a connection string to our Azure Storage account.
+            var connectionString = configuration.GetConnectionString("StorageAccount");
+
+            // Get a reference to the container client object so you can create the "photos" container
+            string containerName = "photos";
+            BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+            container.CreateIfNotExists();
+
+            // Uploads the image to Blob storage.  If a blb already exists with this name it will be overwritten
+            string blobName = "docs-and-friends-selfie-stick";
+            string fileName = "docs-and-friends-selfie-stick.png";
+            BlobClient blobClient = container.GetBlobClient(blobName);
+            blobClient.Upload(fileName, true);
+
+            // List out all the blobs in the container
+            var blobs = container.GetBlobs();
+            foreach (var blob in blobs)
+            {
+                Console.WriteLine($"{blob.Name} --> Created On: {blob.Properties.CreatedOn:yyyy-MM-dd HH:mm:ss}  Size: {blob.Properties.ContentLength}");
+            }
+        }
+    }
+}
